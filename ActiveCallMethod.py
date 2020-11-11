de988f3e341b76017f048333e29bd44968cbc219
Dio <zhendong2011@live.cn>
2020-11-12 05:23:29 +0800

    改变属性值

--- ActiveCallMethod.py
+++ ActiveCallMethod.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-
 import frida, sys
 
-#HOOK 主动调用普通方法
+# HOOK 主动调用普通方法
 jscode = """
 if(Java.available){
     Java.perform(function(){
         //hook动态函数，找到instance实例，从实例调用函数方法
         Java.choose("com.my.fridademo.Util2",{
             onMatch:function(instance){
+                //动态成员变量需要找到实例，给实例设置新值；
+                instance.bool_var.value = true;
+                console.log("After set new value 2 : " + instance.bool_var.value);
+
                 instance.active_call_func("hooked_param 1");
             },onComplete:function(){}
         })
@@ -16,11 +19,13 @@ if(Java.available){
 }
 """
 
+
 def on_message(message, data):
     if message['type'] == 'send':
         print(" {0}".format(message['payload']))
     else:
         print(message)
+
 
 # 查找USB设备并附加到目标进程
 session = frida.get_usb_device().attach('com.my.fridademo')
--- ActiveCallStatic.py
+++ ActiveCallStatic.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
-
 import frida, sys
 
-#HOOK 主动调用静态方法
+# HOOK 主动调用静态方法
 jscode = """
 if(Java.available){
     Java.perform(function(){
         var Util2 = Java.use("com.my.fridademo.Util2");//获取到类
+        //静态成员变量可以直接设置结果
+        Util2.static_bool_var.value = true;//改变静态属性值
+        console.log("After set new value 1:" + Util2.static_bool_var.value);
         Util2.active_static_call_func("param_hooked");
     });
 }
 """
+
 
 def on_message(message, data):
     if message['type'] == 'send':
         print(" {0}".format(message['payload']))
     else:
         print(message)
+
 
 # 查找USB设备并附加到目标进程
 session = frida.get_usb_device().attach('com.my.fridademo')
